e9506f29256a09a32c0d78fb3ed8b488b35b7d7a
givanse <silvag777@gmail.com>
2013-12-09 13:28:56 -0600

    Add the core objects of the app. Driver, Service, etc.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Role.create(name: 'administrador')
-Role.create(name: 'propietario')
-Role.create(name: 'operador')
-Role.create(name: 'conductor')
-Role.create(name: 'cliente')
 
 DriverStatus.create(name: 'incomunicado')
 DriverStatus.create(name: 'inactivo')
 DriverStatus.create(name: 'libre')
 DriverStatus.create(name: 'ocupado')
 
+User.create(name: 'Gaston Silva', phone_number: '6666 7777', email: 'gaston@mail.com', role_id: 1, password: '12345678', password_confirmation: '12345678')
+
+Driver.create(license: '01020304', cellphone: '33 0000 1111', driver_status_id: 3, user_id: 1)
+
+Role.create(name: 'administrador')
+Role.create(name: 'propietario')
+Role.create(name: 'operador')
+Role.create(name: 'conductor')
+Role.create(name: 'cliente')
+
+# TODO: `block in trace_on': invalid byte sequence in US-ASCII
+# For: Eca Do Queirós
+Service.create(address: 'Eca Do Queiros 5210', suburb: 'Jardines Universidad', phone_number: 44449999, latitude: 20.69131, longitude: -103.421765, schedule_at: DateTime.new(2013, 12, 30, 18, 30))
+
+Vehicle.create(plate: 'amj-0000',  model: 'Tsuru',  year: 2010, driver_id: 1)
--- scripts/kaboom.sh
+++ scripts/kaboom.sh
@@ -3,7 +3,11 @@
 set -e
 
 # Shutdown app / server
-# Delete all the migrations
+echo '
+Remminder, possibly do:
+  rm app/admin/*
+  rm db/migrate/*
+' 
 
 ########### Clean ##########
 rake db:drop
@@ -19,14 +23,14 @@ rails destroy devise user
 rails generate devise user 
 rake db:migrate
 # Manually add as accessible fields in the User model
-rails generate migration add_role_id_to_users role_id:integer
-rails generate migration add_name_to_users name:string
-rails generate migration add_phone_number_to_users phone_number:integer
+rails generate migration add_role_id_to_users role_id:integer{1}
+rails generate migration add_name_to_users name:string{50}
+rails generate migration add_phone_number_to_users phone_number:string{16}
 # Models
 rails generate model role name:string{24}
 rails generate model driver_status name:string{12}
-rails generate model driver license:string cellphone:integer driver_status_id:integer
-rails generate model vehicle plate:string model:string year:integer  
+rails generate model driver license:string cellphone:string{16} driver_status_id:integer user_id:integer
+rails generate model vehicle plate:string model:string year:integer{4} driver_id:integer 
 # if bash shell, use dots for precision, http://stackoverflow.com/a/14369874/7852
 rails generate model service address:string suburb:string phone_number:integer latitude:decimal{9.6} longitude:decimal{9.6} schedule_at:datetime 
 rake db:migrate
@@ -45,7 +49,7 @@ rails generate active_admin:resource Service
 # Enable strong parameters in app/admin/* 
 
 # populate
-rake db:seed
+#rake db:seed
 
 echo 'Next steps:
 1.- Relationships, manually add:
