8dfccb14770afe4989c2e43e6dddc50b70b43d89
givanse <silvag777@gmail.com>
2013-12-09 16:37:35 -0600

    correctly enable ES locale

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Corzo
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config/locales/', '*.{rb,yml}').to_s]
+    config.i18n.locale = :es 
     config.i18n.default_locale = :es
 
     # Devise
