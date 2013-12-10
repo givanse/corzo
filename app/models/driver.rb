f1ccf5b1e746df7d3f584f95e12665cba2797d7d
givanse <silvag777@gmail.com>
2013-12-09 18:14:27 -0600

    change Driver display field

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,10 @@
 class Driver < ActiveRecord::Base
+
   belongs_to :user
   belongs_to :driver_status
   has_one :vehicle
+
+  def to_s
+    license  
+  end
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,7 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :driver
+
+  def to_s
+    model + ' ' + year.to_s + ' ' + plate
+  end
 end
