e6eec163ee86264f070bc9ae135113f304490423
givanse <silvag777@gmail.com>
2013-12-10 10:30:17 -0600

    just format

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Corzo::Application.routes.draw do
-  devise_for :users
   ActiveAdmin.routes(self)
+  devise_for :users
+
   #match '/', to: 'static_pages#home', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   # Devise
   # Ensure you have defined root_url to *something* in your config/routes.rb
-  #root :to => 'static_pages#home'
-  root 'static_pages#home'
+  root :to => 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
