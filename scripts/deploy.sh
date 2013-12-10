14f45d26281d331d5d31e75fc4b2a4655d359447
givanse <silvag777@gmail.com>
2013-12-10 03:20:04 -0600

    add a little more DB admin

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,11 @@
 # This should only be used if your Heroku deploy fails without it.
 # rake assets:precompile
 
+# heroku pg:reset DATABASE
+
 git push heroku master
 heroku run rake db:migrate
+
+heroku run rake db:seed
 
 #EOF
