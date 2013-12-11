c0c13917a03b7ea98e516f203f1132748d712963
givanse <silvag777@gmail.com>
2013-12-10 18:50:23 -0600

    edit app's title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = 'Project Corzo'
+    base_title = t(:project_name)
     if page_title.empty?
       base_title
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} :: #{page_title}"
     end
   end
 
