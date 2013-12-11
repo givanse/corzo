8916d2fc4dee4b1d85b9cec59388c07e7f74e931
givanse <silvag777@gmail.com>
2013-12-11 14:07:14 -0600

    customize Users index

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -13,5 +13,23 @@ ActiveAdmin.register User do
   #  permitted << :other if resource.something?
   #  permitted
   # end
+
+  filter :email
+  filter :name
+  filter :phone_number
+  filter :role_id
+  filter :created_at                                                           
+  filter :updated_at                                                           
+
+  index do                                                                       
+    selectable_column                                                            
+    column :id                                                                   
+    column :email                                                             
+    column :name                                                               
+    column :role_id                                                          
+    column :created_at                                                           
+    column :updated_at                                                           
+    default_actions                                                              
+  end 
   
 end
