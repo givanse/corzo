06a5c9ceb461d2c347708d981ef00fe6b5989ee8
givanse <silvag777@gmail.com>
2014-05-29 15:32:50 -0500

    improve documentation

--- scripts/keepMine.sh
+++ scripts/keepMine.sh
@@ -2,6 +2,12 @@
 
 # http://stackoverflow.com/a/930495/7852
 
+# A simple Copy/Merge driver, it does nothing.
+
+# Add the driver:
+# git config merge.keepMine.name "always keep mine during merge"
+# git config merge.keepMine.driver "scripts/keepMine.sh %O %A %B"
+
 # I want to keep MY version when there is a conflict
 # Nothing to do: %A (the second parameter) already contains my version
 # Just indicate the merge has been successfully "resolved" with the exit status
